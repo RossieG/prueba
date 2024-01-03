582e899117657d40591304adecd37b529b718b0b
RossieG <rossiejashiel@gmail.com>
2024-01-03 00:34:26 -0400

    Add app count

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View,Pressable,Button } from 'react-native';
 
-export default function App() {
+
+export default function App()  {
+
+var [cont,setcont] =useState(0); 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize:20}}>Contador </Text>
+      
+      <View style={styles.containerButton}>
+        <View style={styles.button}>
+          <Button title='Contar'  color="#4CAF50"
+                  onPress={()=>setcont(cont +=1)}
+          > </Button>
+        </View>
+        <View style={styles.button}>
+          <Button title='Reset' color="#ff5c5c"
+            onPress={()=>setcont(cont=0)}
+           > </Button>       
+        
+        </View>
+
+      </View>
+      <View>
+        <Text
+         style={{fontSize:52}}
+        >
+          {cont}
+        </Text>
+      </View>
+     
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
@@ -16,5 +44,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  containerButton: {
+    flex: .10 ,
+    flexDirection: 'row',
+    alignItems: 'flex-start', //replace with flex-end or center 
+    justifyContent:'space-between',
+
+  },
+  button:{
+    marginTop:10,
+    marginRight:9,
   },
 });
